751e3605ac56b92c60985e02a137d37827849a90
Isadora Loredo <flisadora@gmail.com>
2018-12-12 12:36:08 +0000

    add images

--- itwMobileApp/App/viewmodels/welcome.js
+++ itwMobileApp/App/viewmodels/welcome.js
@@ -16,7 +16,7 @@
             'Make jQuery & Bootstrap widgets templatable and bindable (or build your own widgets).'
         ];
     };
-
+    //
     //Note: This module exports a function. That means that you, the developer, can create multiple instances.
     //This pattern is also recognized by Durandal so that it can create instances on demand.
     //If you wish to create a singleton, you should export an object instead of a function.
